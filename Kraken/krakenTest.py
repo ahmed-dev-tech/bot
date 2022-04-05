20c4d285fe2bbc5a89664199a9486f472367e0b1
ahmed-dev-tech <76089213+ahmed-dev-tech@users.noreply.github.com>
2022-04-05 19:44:33 +0500

    updated

--- Kraken/krakenTest.py
+++ Kraken/krakenTest.py
@@ -8,14 +8,8 @@ import urllib.parse
 import hashlib
 import hmac
 import base64
-
-def get_kraken_signature(urlpath, data, secret):
-    postdata = urllib.parse.urlencode(data)
-    encoded = (str(data['nonce']) + postdata).encode()
-    message = urlpath.encode() + hashlib.sha256(encoded).digest()
-    mac = hmac.new(base64.b64decode(secret), message, hashlib.sha512)
-    sigdigest = base64.b64encode(mac.digest())
-    return sigdigest.decode()
+# from pykrakenapi import KrakenAPI
+api_url = "https://api.kraken.com"
 # Attaches auth headers and returns results of a POST request
 def kraken_request(uri_path, data, api_key, api_sec):
     headers = {}
@@ -177,5 +171,12 @@ def krakenBot():
             krakenBot()
         else:
             sys.exit()
+def get_kraken_signature(urlpath, data, secret):
+    postdata = urllib.parse.urlencode(data)
+    encoded = (str(data['nonce']) + postdata).encode()
+    message = urlpath.encode() + hashlib.sha256(encoded).digest()
+    mac = hmac.new(base64.b64decode(secret), message, hashlib.sha512)
+    sigdigest = base64.b64encode(mac.digest())
+    return sigdigest.decode()
 if __name__ == "__main__":
     krakenBot()
